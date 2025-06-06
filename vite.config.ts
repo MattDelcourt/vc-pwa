d13c2e10f9dbb66b22f632d987ba1dd862941302
Owner <valorouscreations@outlook.com>
2025-06-06 17:43:57 -0600

    Fixed minor errors with icons

--- vite.config.ts
+++ vite.config.ts
@@ -16,12 +16,12 @@
             display: 'standalone',
             icons: [
               {
-                src: '/icon-192.svg',
+                src: '/icon-192.png',
                 sizes: '192x192',
                 type: 'image/png'
               },
               {
-                src: '/icon-512.svg',
+                src: '/icon-512.png',
                 sizes: '512x512',
                 type: 'image/png'
               }
